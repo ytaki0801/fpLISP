a0836cbff53b4eed52ad2eb01c7cc9826e52265b
TAKIZAWA Yozo <y@nbk.bz>
2021-12-07 16:57:35 +0900

    Redefined apply function

--- Scheme/fpLISP.scm
+++ Scheme/fpLISP.scm
@@ -10,73 +10,64 @@
 (define (fp_eq x y)
   (let ((NIL `(,#f nil ())) (T `(,#t t)))
     (cond ((and (member x NIL) (member y NIL)) #t)
-	  ((and (member x T)   (member y T)    #t))
-	  (else (eq? x y)))))
+          ((and (member x T)   (member y T)    #t))
+          (else (eq? x y)))))
 
+;;;; Define built-in functions
+(define fp_builtins
+  `((cons . ,cons) (car . ,car) (cdr . ,cdr) (+ . ,+) (- . ,-) (* . ,*)
+    (eq . ,fp_eq) (atom . ,(lambda (x) (not (pair? x)))) (lt . ,<)
+    (/ . ,(lambda (x y) (exact->inexact (/ x y)))) (% . ,remainder)))
+   
 ;;;; Apply a function with arguments for a lambda expression
 ;;;; and built-in functions
 (define (fp_apply f v)
-  (cond ((pair? f)
-	 (if (eq? (car f) 'lambda)
-	     ; Eval body of a lambda expression in local env
-	     ; made from variables, values and closure env
-	     (let ((lvars (cadr f)))
-	       (fp_eval
-		(caddr f)
-		(append (cond ((null? lvars) '())
-			      ((pair? lvars) (map cons lvars v))
-			      (else `(,(cons lvars v))))
-		        (cadddr f))))
-	     #f))
-	((eq? f 'cons) (cons (car v) (cadr v)))
-	((eq? f 'car)  (car (car v)))
-	((eq? f 'cdr)  (cdr (car v)))
-	((eq? f 'eq)   (fp_eq (car v) (cadr v)))
-	((eq? f 'atom) (not (pair? (car v))))
-	((eq? f '+)    (+ (car v) (cadr v)))
-	((eq? f '-)    (- (car v) (cadr v)))
-	((eq? f '*)    (* (car v) (cadr v)))
-	((eq? f '/)    (exact->inexact (/ (car v) (cadr v))))
-	((eq? f '%)    (remainder (car v) (cadr v)))
-	((eq? f 'lt)   (< (car v) (cadr v)))
-	(else #f)))
-
-;;;; List of built-in function and Boolean names
-(define fp_builtins '(cons car cdr eq atom + - * / % lt t nil))
+  (if (pair? f)
+      (if (eq? (car f) 'lambda)
+          ; Eval body of a lambda expression in local env
+          ; made from variables, values and closure env
+          (let ((lvars (cadr f)))
+            (fp_eval
+              (caddr f)
+              (append (cond ((null? lvars) '())
+                            ((pair? lvars) (map cons lvars v))
+                            (else `(,(cons lvars v))))
+                      (cadddr f))))
+          #f)
+      (apply (cdr (assq f fp_builtins)) v)))
 
 ;;;; Look up value for a name
 (define (fp_lookup t a)
-  (cond ((or (member t fp_builtins) (number? t)) t)
-	(else (cdr (assq t a)))))
+  (cond ((or (member t (map car fp_builtins))
+             (number? t) (eq? t 't) (eq? t 'nil)) t)
+        (else (cdr (assq t a)))))
 
 ;;;; Eval S-expression with local environment
 (define (fp_eval e a)
   (cond ((pair? e)
-	 (cond ((eq? (car e) 'quote) (cadr e))
-	       ((eq? (car e) 'if)
-		(if (fp_eq (fp_eval (cadr e) a) 'nil)
-		    (fp_eval (cadddr e) a)
-		    (fp_eval (caddr e) a)))
-	       ((eq? (car e) 'lambda) (append e `(,a)))
-	       (else
-		(fp_apply (fp_eval (car e) a)
-			  (map (lambda (x) (fp_eval x a))
-			       (cdr e))))))
-	(else (fp_lookup e a))))
+         (cond ((eq? (car e) 'quote) (cadr e))
+               ((eq? (car e) 'if)
+                (if (fp_eq (fp_eval (cadr e) a) 'nil)
+                    (fp_eval (cadddr e) a)
+                    (fp_eval (caddr e) a)))
+               ((eq? (car e) 'lambda) (append e `(,a)))
+               (else
+                (fp_apply (fp_eval (car e) a)
+                          (map (lambda (x) (fp_eval x a))
+                               (cdr e))))))
+        (else (fp_lookup e a))))
 
 ;;;; Display S-expression by following the specification of fpLISP
 (define (fp_strcons s)
   (fp_display (car s))
   (let ((sd (cdr s)))
     (cond ((fp_eq sd 'nil) (display ""))
-	  ((pair? sd) (display " ") (fp_strcons sd))
+          ((pair? sd) (display " ") (fp_strcons sd))
           (else (display " . ") (fp_display sd)))))
 (define (fp_display s)
-  (cond ((fp_eq s 'nil) (display 'nil))
-	((fp_eq s #t)   (display 't))
-	((fp_eq s #f)   (display 'nil))
-	((pair? s) (display "(") (fp_strcons s) (display ")"))
-	(else (display s))))
+  (cond ((fp_eq s #t) (display 't)) ((fp_eq s #f) (display 'nil))
+        ((pair? s) (display "(") (fp_strcons s) (display ")"))
+        (else (display s))))
 
 (fp_display (fp_eval (read) '()))
 (newline)
